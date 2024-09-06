52d957d224a729cd642fb817c306c374ad30ecda
Abhiram <Abhiram@agpl.net>
2024-09-06 11:22:47 +0530

    optimize

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return "Cache is cleared";
+});
+Route::get('migrate', function () {
+    Artisan::call('migrate');
+    return "Migrate Completed!";
+});
+Route::get('optimize', function () {
+    Artisan::call('optimize');
+    return "optimized!";
+});
